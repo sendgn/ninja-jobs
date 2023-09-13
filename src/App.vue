625115245e1fc07e1bb4cf09824f49984738a68e
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-13 19:31:51 +0300

    Programmatic Navigation

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,29 @@
     <router-link :to="{ name: 'AboutView' }">About</router-link>
     <router-link :to="{ name: 'JobsView' }">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'HomeView' })
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -19,7 +40,6 @@
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
@@ -27,9 +47,16 @@ nav a {
   padding: 10px;
   border-radius: 4px;
 }
-
 nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
