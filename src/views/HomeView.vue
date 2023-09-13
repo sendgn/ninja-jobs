1932de725c2c89f106f8d3adbb581da8492c8c8d
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-13 09:36:51 +0300

    Get rid of unnecessary code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa recusandae veniam laudantium fugiat veritatis omnis quasi. Tempora quo assumenda doloremque quibusdam debitis, alias aperiam facilis amet, officiis numquam omnis praesentium fugiat et exercitationem voluptate quas iusto, obcaecati sunt minus sequi aut enim rerum. Aut rem voluptatem neque explicabo numquam libero nostrum officia eveniet fugiat dicta, asperiores saepe. Ipsam id ratione quibusdam sit amet, exercitationem sunt. Minus ex odit similique, quo sapiente maiores fugiat non atque neque esse, vel laborum perferendis animi nihil soluta repellat deleniti illum earum, eum itaque magnam? Nobis officiis consectetur beatae facere aperiam nemo placeat, nam voluptate!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi corrupti ut, assumenda voluptatem quibusdam illo itaque, ea praesentium iusto nam nisi ducimus, cumque incidunt asperiores pariatur provident nulla illum. Omnis, quaerat odit aspernatur ad quo, consequuntur ratione, iste ducimus enim voluptas assumenda. Reprehenderit quibusdam fugiat sequi earum dolorum deserunt vel soluta enim blanditiis quos repellendus aut iusto qui rerum aspernatur harum, rem quae obcaecati labore optio suscipit tempore quisquam ipsa consectetur? Enim quae officiis quo ut quisquam laborum pariatur eum, dolorem esse, dolore impedit quasi? At, optio fuga consequatur neque suscipit veritatis qui perspiciatis magnam quibusdam. Nesciunt quasi illo omnis?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
